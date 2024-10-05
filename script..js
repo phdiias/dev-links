7271b904a545d1a53917081bec7cb1baa80a37c9
Pedro Henrique <pedropd.barbosa@gmail.com>
2024-10-04 22:10:57 -0300

    update img profile

--- script..js
+++ script..js
@@ -1,16 +1,12 @@
 function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
+  const html = document.documentElement;
+  html.classList.toggle('light');
 
   // trocar a imagem baseado no tema
-  const img = document.querySelector("#profile img")
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute(
-      "alt",
-      "foto do Pedro com um terno preto e o celular na mão."
-    )
+  const img = document.querySelector('#profile img');
+  if (html.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar.png');
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute('src', './assets/avatar.png');
   }
 }
